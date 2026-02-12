1b6473b4f5369f71fc3ba4b3cd2bc3a17f2c9f34
ongunumutyelbasi <umutyelbasi@gmail.com>
2026-02-12 23:40:57 +0000

    Changes.

--- app/(payload)/admin/[[...segments]]/page.tsx
+++ app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { RootPage } from '@payloadcms/next/views'
 import payloadConfig from '../../../../payload.config'
 // @ts-ignore
